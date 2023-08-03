9136f8175cf3c04244eefc3fe8fe113aaae199f0
ShashankOO7 <Shashankchourasia7@gmail.com>
2023-08-03 18:05:23 +0530

    MERN

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -25,7 +25,7 @@ const SignUp=()=>{
         
         if(firstName && lastName && email && (password = confirmPassword))
         {
-            axios.post('http://localhost/4000/signup', data)
+            axios.post('http://localhost:4000/signup', data)
             .then((res)=>{
                 alert(res.data.message)
                 navigate('/')
